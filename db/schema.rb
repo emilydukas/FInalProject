98ad9cabe4464bceb955381c9f353f088204d42a
Urvish Patel <urvishp13@gmail.com>
2015-06-09 22:09:59 +0200

    working on login page

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150609085824) do
+ActiveRecord::Schema.define(version: 20150609190829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150609085824) do
     t.text     "phone_number"
     t.string   "email_address"
     t.string   "image_url"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20150609085824) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.text     "phone_number"
+    t.string   "name"
+    t.string   "image_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
