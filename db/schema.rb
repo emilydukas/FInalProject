e8c8e20d3aa98d38818c8f1d85be9020ba85b7aa
Urvish Patel <urvishp13@gmail.com>
2015-06-10 14:42:20 +0200

    pushing my stuff

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150610120521) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "board_id"
+    t.integer  "user_id"
     t.text     "phone_number"
     t.string   "email_address"
     t.string   "image_url"
@@ -47,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150610120521) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.text     "phone_number"
     t.string   "name"
     t.string   "image_url"
     t.string   "avatar_file_name"
