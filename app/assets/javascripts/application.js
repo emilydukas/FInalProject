0b25906eeca8eb392ce83fefa24cf025fe01f37f
emilydukas <ed4da@virginia.edu>
2015-06-11 12:05:37 +0200

    Bootstrap working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require bootply
-//= require scripts
 //= require turbolinks
+//= require scripts
 //= require_tree .
